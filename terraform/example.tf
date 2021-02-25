8e95dbd4ab846ab0559c59512c3c0e4330487888
matthewmacklin <matthew.macklin@jisc.ac.uk>
2021-02-25 10:07:39 +0000

    Added buildspec

--- terraform/example.tf
+++ terraform/example.tf
@@ -95,6 +95,8 @@ resource "aws_codebuild_project" "example-cbm" {
     type            = "GITHUB"
     location        = "https://github.com/matthewmacklin/terraform-cbm"
     git_clone_depth = 1
+    buildspec       = "./build/deploy-to-prod-buildspec.yml"
+
 
     git_submodules_config {
       fetch_submodules = true
@@ -116,46 +118,6 @@ resource "aws_codebuild_project" "example-cbm" {
 #     #   aws_security_group.example2.id,
 #     ]
 #   }
-
-  tags = {
-    Environment = "Test"
-  }
-}
-
-resource "aws_codebuild_project" "project-with-cache" {
-  name           = "test-project-cache"
-  description    = "test_codebuild_project_cache"
-  build_timeout  = "5"
-  queued_timeout = "5"
-
-  service_role = aws_iam_role.example-cbm.arn
-
-  artifacts {
-    type = "NO_ARTIFACTS"
-  }
-
-  cache {
-    type  = "LOCAL"
-    modes = ["LOCAL_DOCKER_LAYER_CACHE", "LOCAL_SOURCE_CACHE"]
-  }
-
-  environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:1.0"
-    type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "CODEBUILD"
-
-    environment_variable {
-      name  = "SOME_KEY1"
-      value = "SOME_VALUE1"
-    }
-  }
-
-  source {
-    type            = "GITHUB"
-    location        = "https://github.com/mitchellh/packer.git"
-    git_clone_depth = 1
-  }
 
   tags = {
     Environment = "Test"
