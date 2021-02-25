206980ddf0b585cabf1a0e5f0546b0f45cb8b605
matthewmacklin <matthew.macklin@jisc.ac.uk>
2021-02-25 16:12:07 +0000

    update

--- terraform/example.tf
+++ terraform/example.tf
@@ -1,144 +1,144 @@
-provider "aws" {
-  region = "eu-west-2"
-}
+# provider "aws" {
+#   region = "eu-west-2"
+# }
 
-resource "aws_s3_bucket" "example-cbm" {
-  bucket = "example-cbm"
-  acl    = "private"
-}
+# resource "aws_s3_bucket" "example-cbm" {
+#   bucket = "example-cbm"
+#   acl    = "private"
+# }
 
-resource "aws_iam_role" "example-cbm" {
-  name = "example-cbm"
+# resource "aws_iam_role" "example-cbm" {
+#   name = "example-cbm"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "codebuild.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": "codebuild.amazonaws.com"
+#       },
+#       "Action": "sts:AssumeRole"
+#     }
+#   ]
+# }
+# EOF
+# }
 
-resource "aws_iam_role_policy" "example-cbm" {
-  role = aws_iam_role.example-cbm.name
+# resource "aws_iam_role_policy" "example-cbm" {
+#   role = aws_iam_role.example-cbm.name
 
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "*"
-      ],
-      "Resource": [
-        "*"
-      ]}
-  ]
-}
-POLICY
-}
+#   policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Action": [
+#         "*"
+#       ],
+#       "Resource": [
+#         "*"
+#       ]}
+#   ]
+# }
+# POLICY
+# }
 
-resource "aws_codebuild_project" "example-cbm" {
-  name          = "test-project"
-  description   = "test_codebuild_project"
-  build_timeout = "5"
-  service_role  = aws_iam_role.example-cbm.arn
+# resource "aws_codebuild_project" "example-cbm" {
+#   name          = "test-project"
+#   description   = "test_codebuild_project"
+#   build_timeout = "5"
+#   service_role  = aws_iam_role.example-cbm.arn
 
-  artifacts {
-    type = "NO_ARTIFACTS"
-  }
-
-  cache {
-    type     = "S3"
-    location = aws_s3_bucket.example-cbm.bucket
-  }
-
-  environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:1.0"
-    type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "CODEBUILD"
-
-    # environment_variable {
-    #   name  = "SOME_KEY1"
-    #   value = "SOME_VALUE1"
-    # }
-
-    # environment_variable {
-    #   name  = "SOME_KEY2"
-    #   value = "SOME_VALUE2"
-    #   type  = "PARAMETER_STORE"
-    # }
-  }
-
-  logs_config {
-    cloudwatch_logs {
-      group_name  = "log-group"
-      stream_name = "log-stream"
-    }
-
-    s3_logs {
-      status   = "ENABLED"
-      location = "${aws_s3_bucket.example-cbm.id}/build-log"
-    }
-  }
-
-  source {
-    type            = "GITHUB"
-    location        = "https://github.com/matthewmacklin/terraform-cbm"
-    git_clone_depth = 1
-    buildspec       = "./codebuild/buildspec.yml"
-
-
-    git_submodules_config {
-      fetch_submodules = true
-    }
-  }
-
-  source_version = "master"
-
-#   vpc_config {
-#     vpc_id = aws_vpc.example-cbm.id
-
-#     subnets = [
-#     #   aws_subnet.example1.id,
-#     #   aws_subnet.example2.id,
-#     ]
-
-#     security_group_ids = [
-#     #   aws_security_group.example1.id,
-#     #   aws_security_group.example2.id,
-#     ]
+#   artifacts {
+#     type = "NO_ARTIFACTS"
 #   }
 
-  tags = {
-    Environment = "Test"
-  }
-}
+#   cache {
+#     type     = "S3"
+#     location = aws_s3_bucket.example-cbm.bucket
+#   }
+
+#   environment {
+#     compute_type                = "BUILD_GENERAL1_SMALL"
+#     image                       = "aws/codebuild/standard:1.0"
+#     type                        = "LINUX_CONTAINER"
+#     image_pull_credentials_type = "CODEBUILD"
+
+#     # environment_variable {
+#     #   name  = "SOME_KEY1"
+#     #   value = "SOME_VALUE1"
+#     # }
+
+#     # environment_variable {
+#     #   name  = "SOME_KEY2"
+#     #   value = "SOME_VALUE2"
+#     #   type  = "PARAMETER_STORE"
+#     # }
+#   }
+
+#   logs_config {
+#     cloudwatch_logs {
+#       group_name  = "log-group"
+#       stream_name = "log-stream"
+#     }
+
+#     s3_logs {
+#       status   = "ENABLED"
+#       location = "${aws_s3_bucket.example-cbm.id}/build-log"
+#     }
+#   }
+
+#   source {
+#     type            = "GITHUB"
+#     location        = "https://github.com/matthewmacklin/terraform-cbm"
+#     git_clone_depth = 1
+#     buildspec       = "./codebuild/buildspec.yml"
+
+
+#     git_submodules_config {
+#       fetch_submodules = true
+#     }
+#   }
+
+#   source_version = "master"
+
+# #   vpc_config {
+# #     vpc_id = aws_vpc.example-cbm.id
+
+# #     subnets = [
+# #     #   aws_subnet.example1.id,
+# #     #   aws_subnet.example2.id,
+# #     ]
+
+# #     security_group_ids = [
+# #     #   aws_security_group.example1.id,
+# #     #   aws_security_group.example2.id,
+# #     ]
+# #   }
+
+#   tags = {
+#     Environment = "Test"
+#   }
+# }
 
 
 
 
-data "aws_secretsmanager_secret_version" "creds" {
-  secret_id = "CBM_GH_TOKEN"
-}
+# data "aws_secretsmanager_secret_version" "creds" {
+#   secret_id = "CBM_GH_TOKEN"
+# }
 
-locals {
-  gh_credentials = jsondecode(
-    data.aws_secretsmanager_secret_version.creds.secret_string
-  )
-}
+# locals {
+#   gh_credentials = jsondecode(
+#     data.aws_secretsmanager_secret_version.creds.secret_string
+#   )
+# }
 
-resource "aws_codebuild_source_credential" "github_token" {
-  auth_type   = "PERSONAL_ACCESS_TOKEN"
-  server_type = "GITHUB"
-  token       = local.gh_credentials.cbm_gh_token
-}
+# resource "aws_codebuild_source_credential" "github_token" {
+#   auth_type   = "PERSONAL_ACCESS_TOKEN"
+#   server_type = "GITHUB"
+#   token       = local.gh_credentials.cbm_gh_token
+# }
---
+++ terraform/main2.tf
@@ -0,0 +1,126 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "oa-sherpa-tfstate"
+    key    = "app-state"
+    region = "eu-west-2"
+    encrypt = true
+  }
+}
+
+resource "aws_iam_role" "cbm_codebuild" {
+  name = "sherpa_codebuild"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": ["codebuild.amazonaws.com",
+                    "events.amazonaws.com"
+        ]
+        
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy" "cbm_codebuild_policy" {
+  role = aws_iam_role.sherpa_codebuild.name
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Resource": [
+              "*"
+            ],
+            "Action": [
+                "sns:*",
+                "ssm:*",
+                "kms:*",
+                "rds:*",
+                "s3:*",
+                "firehose:*",
+                "logs:*",
+                "cloudformation:*",
+                "elasticloadbalancing:*",
+                "autoscaling-plans:*",
+                "es:*",
+                "ecr:*",
+                "apigateway:*",
+                "dynamodb:*",
+                "lambda:*",
+                "events:*",
+                "sqs:*",
+                "autoscaling:*",
+                "iam:*",
+                "elasticbeanstalk:*",
+                "secretsmanager:*",
+                "route53:*",
+                "ec2:*",
+                "ram:*",
+                "kinesis:*",
+                "codebuild:*",
+                "cloudwatch:*",
+                "elasticfilesystem:*"
+            ]
+        }
+    ]
+  }
+  POLICY
+}
+
+
+data "aws_secretsmanager_secret_version" "creds" {
+  secret_id = "CBM_GH_TOKEN"
+}
+
+locals {
+  gh_credentials = jsondecode(
+    data.aws_secretsmanager_secret_version.creds.secret_string
+  )
+}
+
+resource "aws_codebuild_source_credential" "github_token" {
+  auth_type   = "PERSONAL_ACCESS_TOKEN"
+  server_type = "GITHUB"
+  token       = local.gh_credentials.cbm_gh_token
+}
+
+
+resource "aws_codebuild_project" "cbm-test" {
+  name          = "deploy-prod"
+  description   = "Runs tests for sherpa search and elasticsearch, and deploys to prod if they pass"
+  build_timeout = "120"
+  service_role  = aws_iam_role.sherpa_codebuild.arn
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_LARGE"
+    image                       = "aws/codebuild/standard:5.0"
+    type                        = "LINUX_CONTAINER"
+    privileged_mode             = true
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = "https://github.com/matthewmacklin/terraform-cbm"
+    git_clone_depth = 1
+    buildspec       = "./codebuild/buildspec.yml"
+  }
+  source_version = "master"
+}
