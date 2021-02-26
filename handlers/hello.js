2753d5605bc2185a5f70b08b0c9394b3ddae4acb
matthewmacklin <matthew.macklin@jisc.ac.uk>
2021-02-26 16:42:52 +0000

    changed hello

--- handlers/hello.js
+++ handlers/hello.js
@@ -5,7 +5,7 @@ export const main = async (event, context) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Hello world!!! testing 3.53',
+        message: 'Hello world!!! testing 4.15',
       },
       null,
       2
