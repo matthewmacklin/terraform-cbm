93a86f697106a4dd2b2f59996e58fb2d7a4745b4
matthewmacklin <matthew.macklin@jisc.ac.uk>
2021-03-01 12:32:36 +0000

    updated message

--- handlers/hello.js
+++ handlers/hello.js
@@ -5,7 +5,7 @@ export const main = async (event, context) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Hello world!!! testing uat stage 1 Mar 10.56',
+        message: 'Hello world!!! testing uat stage 1 Mar 12.32',
       },
       null,
       2
