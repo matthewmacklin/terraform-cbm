d65c63e2c8b7a9441507816253aa817b30f25290
matthewmacklin <matthew.macklin@jisc.ac.uk>
2021-03-01 12:44:49 +0000

    changed hello messsage

--- handlers/hello.js
+++ handlers/hello.js
@@ -5,7 +5,7 @@ export const main = async (event, context) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Hello world!!! testing uat stage 1 Mar 12.32',
+        message: 'Hello world! Deploying to prod from codebuild.',
       },
       null,
       2
